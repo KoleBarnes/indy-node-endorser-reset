e0e356293efc9c05062312c9720cec9e557e8627
KoleBarnes <kbarnes261@gmail.com>
2022-04-04 15:58:43 -0700

    Revered refactor.
    Added:
     - pip upgrade to Dockerfile in order to get the latest version of indy-vdr.
     - Collecting info for seqNo, did, role, alias, endpoint, txnTime(converted to date time) in csv through API calls to indy-scan and requests from indy-vdr.
     - Comments.
    
    Changed:
     - requirements.txt indy-vdr to get the lastest version.
    
    Fixed:
     - indyScanNetworkId in networks.json for builderNet and mainNet.
     - Fixed batching. Will now stop looking to txn after number specified runs out.
     - Fixed seqNo saving a 1 in json log. will now save the last txn number retrieved.
     - Fixed seqNo resetting to zero when skipping allow DID's.

--- demote-nyms/demote_nyms.py
+++ demote-nyms/demote_nyms.py
@@ -6,7 +6,9 @@ import requests
 from indy_vdr.ledger import (
     build_get_nym_request,
     build_nym_request,
-    build_custom_request
+    build_custom_request,
+    build_get_txn_request,
+    LedgerType,
 )
 from networks import Network
 from DidKey import DidKey
@@ -25,7 +27,7 @@ class DemoteNyms(object, metaclass=Singleton):
         self.DEMOTE = DEMOTE
         self.role = role
         if batch: self.batch = int(batch)
-        else: self.batch = None
+        else: self.batch = -1
         self.pool_collection = pool_collection
         self.log_path = "./logs/"
 
@@ -36,91 +38,27 @@ class DemoteNyms(object, metaclass=Singleton):
         req = build_get_nym_request(None, nym)
         return await pool.submit_request(req)
 
+    async def get_txn(self, pool, seq_no: int):
+        req = build_get_txn_request(None, LedgerType.DOMAIN, seq_no)
+        return await pool.submit_request(req)
+
     async def demote_nym(self, pool, ident: DidKey, dest):
         """
         Uses build_nym_request to demote NYM.
         """
-        pass
         print("DEMOTING DID!!!")
         nym_build_req = build_nym_request(ident.did, dest)
         nym_build_req_body = json.loads(nym_build_req.body)
-        # util.log_debug(json.dumps(nym_build_req_body, indent=2)) #* Debug
+        #* Debug util.log_debug(json.dumps(nym_build_req_body, indent=2))
 
         nym_build_req_body = json.loads(nym_build_req.body)
         nym_build_req_body["operation"]["role"] = None
         custom_req = build_custom_request(nym_build_req_body)
-        # util.log_debug(json.dumps(json.loads(custom_req.body), indent=2)) #* Debug
+        #* Debug util.log_debug(json.dumps(json.loads(custom_req.body), indent=2))
 
         ident.sign_request(custom_req)
-        # util.log_debug(json.dumps(json.loads(custom_req.body), indent=2)) #* Debug
+        #* Debug util.log_debug(json.dumps(json.loads(custom_req.body), indent=2))
         return await pool.submit_request(custom_req)
-
-    async def iterate(self, pool, ident: DidKey, ALLOW_DIDS_LIST: list, seqNo, demoted_dids_list: list, skipped_dids_list: list, list_of_dids: list):
-        """
-        Iterate through list of DID's and demotes them skipping allowed DIDs
-        """
-        demoted_dids_dict = {}
-
-        for did in list_of_dids:
-            if did in ALLOW_DIDS_LIST:
-                if did not in skipped_dids_list:
-                    skipped_dids_list.append(did)
-                util.info(f'Found Allow DID: {did} Skipping ...')
-                continue
-
-            nym_response = await self.get_nym(pool, did)
-            nym_check = json.loads(nym_response['data']) # Remove json encoding
-            seqNo = nym_check['seqNo']
-            did = nym_check['dest']
-            role = nym_check['role']
-            txnTime = nym_check['txnTime']
-            if 'alias' in nym_check:
-                util.info('yes alias.')
-                alias = nym_check['alias']
-            else:
-                alias = None
-
-            # indyscan_response = requests.get(self.INDYSCAN_BASE_URL + f'/{network.indy_scan_network_id}/ledgers/domain/txs?seqNoGte={seqno_gte}&filterTxNames=[%22ATTRIB%22]&search={did}&sortFromRecent=false')
-            # indyscan_response = indyscan_response.json()
-            # # util.log_debug(json.dumps(indyscan_response, indent=2)) #* Debug
-
-            # if indyscan_response:
-            #     for item in indyscan_response:
-            #         txn = item['txn']['data']
-            #         if endpoint in txn:
-            #             endpoint = txn['endpoint']
-            #         else:
-            #             util.info("No end point.")
-                        
-            # else:
-            #     util.info("No attrib ...")
-
-            row = (seqNo, did, role, alias, txnTime)
-            csv_file_path = self.log_path + 'CSVlog' + '.csv'
-            with open(csv_file_path,'a') as csv_file:
-                writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONE)
-                writer.writerow(row)
-
-            if nym_check['role'] == "101":
-                util.info(f'Building demote request for: {did} SeqNo: {seqNo} Role: {role} ...')
-            else:
-                util.log_debug(f'{did} Not endorser. Role: {role} ...')
-                continue
-
-            if self.DEMOTE and self.role == '101': # Check to avoid accidental demotion.
-                demote_nym_reponse = await self.demote_nym(pool, ident, did)
-                # util.log_debug(json.dumps(demote_nym_reponse, indent=2)) #* Debug
-                new_txn_seqNo = demote_nym_reponse['txnMetadata']['seqNo']
-                demoted_dids_dict['new_txn_seqno'] = new_txn_seqNo
-                demoted_dids_dict['did'] = did
-                demoted_dids_list.append(demoted_dids_dict.copy())
-            
-            if self.batch:
-                if self.batch == 0:
-                    break
-                self.batch = self.batch - 1
-
-        return demoted_dids_list, skipped_dids_list, seqNo
 
     async def demote(self, network: Network, ident: DidKey):
         """
@@ -132,6 +70,7 @@ class DemoteNyms(object, metaclass=Singleton):
         seqNo = 0
         seqno_gte = 0
         data = None
+        list_of_dids = []
         skipped_dids_list = []
         demoted_dids_list = []
         start_time = datetime.datetime.now()
@@ -155,7 +94,7 @@ class DemoteNyms(object, metaclass=Singleton):
         util.info(f'Looking for transaction greater than: {seqno_gte}')
 
         allow_dids_records = util.fetch_allow_dids()
-        # util.log_debug(json.dumps(allow_dids_records, indent=2)) #* Debug
+        #* Debug util.log_debug(json.dumps(allow_dids_records, indent=2))
         for record in allow_dids_records['records']:
             allow_did = record['fields'].get('DIDs')
             ALLOW_DIDS_LIST.append(allow_did)
@@ -163,34 +102,33 @@ class DemoteNyms(object, metaclass=Singleton):
         pool, network_name = await self.pool_collection.get_pool(network.id)
 
         util.info("Starting scan. This may take a while ...")
-        # Check Allow list from previous run to see if they need to be removed.
+
+        # Check Allow list DID's from previous run to see if they need to be removed. Append them to a list to check current state from ledger.
         util.info("Checking allow DIDs from local file ...")
         if data:
-            list_of_dids = []
             util.info("Found allows from previous run. Checking to see if they are still allowed ...")
             if 'allow_dids' in data:
                 for did in data['allow_dids']['dids']:
                     list_of_dids.append(did)
-                
-            demoted_dids_list, skipped_dids_list, seqNo = await self.iterate(pool, ident, ALLOW_DIDS_LIST, seqno_gte, demoted_dids_list, skipped_dids_list, list_of_dids)
         else:
             util.info("No allow DIDs in local file ...")
 
         # Check IndyScan's transactions.
         util.info("Checking IndyScan Transactions ...")
-        list_of_dids = []
         while True:
+            # Get Nym transactions with specified role from indy scan
             util.log_debug(f'Looking for seqNos greater than {seqno_gte} ...')
             indyscan_response = requests.get(self.INDYSCAN_BASE_URL + f'/{network.indy_scan_network_id}/ledgers/domain/txs?seqNoGte={seqno_gte}&filterTxNames=[%22NYM%22]&search={self.role}&sortFromRecent=false')
             #print(self.INDYSCAN_BASE_URL + f'/{network.indy_scan_network_id}/ledgers/domain/txs?seqNoGte={seqno_gte}&filterTxNames=[%22NYM%22]&search={self.role}&sortFromRecent=false')
             indyscan_response = indyscan_response.json()
-            #util.log_debug(json.dumps(indyscan_response, indent=2)) #* Debug
+            #* Debug util.log_debug(json.dumps(indyscan_response, indent=2))
 
             if network_name == "Local von-network":
                 if seqNo + 1 == seqno_gte:
                     util.log_debug('End of local txn ... ')
                     break
-
+            
+            # Add NYM's DID with specified role, append them to a list to check current state from ledger.
             if indyscan_response:
                 for item in indyscan_response:
                     txn = item['idata']['expansion']['idata']['txn']['data']
@@ -199,10 +137,89 @@ class DemoteNyms(object, metaclass=Singleton):
             else:
                 util.info("No more transactions at this time ...")
                 break
+
+            # Iterate through list_of_dids and check the current state on the ledger. Get info and demote if not allowed. 
+            demoted_dids_dict = {}
+            for did in list_of_dids:
+                # Check DID state from ledger. Gather info.
+                # Get NYM info
+                nym_response = await self.get_nym(pool, did)
+                nym_check = json.loads(nym_response['data']) # Remove json encoding
+                seqNo = nym_check['seqNo']
+                did = nym_check['dest']
+                role = nym_check['role']
+                txnTime = nym_check['txnTime']
+                txn_date_time = datetime.datetime.fromtimestamp(txnTime)  
+
+                # Get transaction info for alias.
+                txn_response = await self.get_txn(pool, seqNo)
+                if 'alias' in txn_response['data']['txn']['data']:
+                    alias = txn_response['data']['txn']['data']['alias']
+                else:
+                    alias = None
+
+                # Get endpoint from ATTRIB transaction.
+                indyscan_response = requests.get(self.INDYSCAN_BASE_URL + f'/{network.indy_scan_network_id}/ledgers/domain/txs?filterTxNames=[%22ATTRIB%22]&search={did}&sortFromRecent=true')
+                indyscan_response = indyscan_response.json()
+                #* Debug util.log_debug(json.dumps(indyscan_response, indent=2)) 
+
+                if indyscan_response:
+                    for item in indyscan_response:
+                        txn = item['idata']['expansion']['idata']['txn']['data']
+                        if 'endpoint' in txn:
+                            endpoint = txn['endpoint']
+                            break
+                        else:
+                            endpoint = None
+                            break
+                else:
+                    endpoint = None
+
+                # Write data to csv file.
+                row = (seqNo, did, role, alias, endpoint, txn_date_time)
+                csv_file_path = self.log_path + 'CSVlog' + '.csv'
+                with open(csv_file_path,'a') as csv_file:
+                    writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONE)
+                    writer.writerow(row)
+                util.info(f'Info from {did} collected in CSV log.')
+
+                # Skip Allowed DID's
+                if did in ALLOW_DIDS_LIST:
+                    if did not in skipped_dids_list:
+                        skipped_dids_list.append(did)
+                    util.info(f'Found Allow DID: {did} Skipping ...')
+                    continue
+
+                # Demote if condisions allow. Collect the seqNo of the demote transaction and the DID that was demoted.
+                if self.DEMOTE and self.role == '101': # Check to avoid accidental demotion.
+                    
+                    # Demote if not allowed and role is set as endorsor. 
+                    if nym_check['role'] == "101":
+                        util.info(f'Building demote request for: {did} SeqNo: {seqNo} Role: {role} ...')
+                    else:
+                        util.log_debug(f'{did} Not endorser. Role: {role} ...')
+                        continue
+
+                    demote_nym_reponse = await self.demote_nym(pool, ident, did)
+                    #* Debug util.log_debug(json.dumps(demote_nym_reponse, indent=2))
+                    new_txn_seqNo = demote_nym_reponse['txnMetadata']['seqNo']
+                    demoted_dids_dict['new_txn_seqno'] = new_txn_seqNo
+                    demoted_dids_dict['did'] = did
+                    demoted_dids_list.append(demoted_dids_dict.copy())
                 
-            demoted_dids_list, skipped_dids_list, seqNo = await self.iterate(pool, ident, ALLOW_DIDS_LIST, seqno_gte, demoted_dids_list, skipped_dids_list, list_of_dids)
+                if self.batch != -1:
+                    if self.batch == 1:
+                        break
+                    self.batch = self.batch - 1
+                # End of for loop.
+
+            if self.batch != -1 and self.batch == 1:
+                util.log("Hit batch limit. Finishing up ...")
+                break
 
             seqno_gte = seqNo + 1 # Get the last seqNo from the last indyscan response
+            list_of_dids = []
+            # end of while loop
 
         # Build Json Log
         end_time = datetime.datetime.now()
@@ -210,7 +227,10 @@ class DemoteNyms(object, metaclass=Singleton):
         result['end_time'] = str(end_time)
         result['time_delta'] = str(end_time - start_time)
         result['executing_did'] = ident.did
-        result['last_seqNo'] = seqNo + 1
+        if seqNo == 0:
+            result['last_seqNo'] = seqno_gte
+        else:
+            result['last_seqNo'] = seqNo + 1
         if skipped_dids_list:
             result['allow_dids'] = {'count': len(skipped_dids_list), 'dids': skipped_dids_list }
         if demoted_dids_list:
